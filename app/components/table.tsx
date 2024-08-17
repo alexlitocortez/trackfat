efd589846e7edb48bcf9fedfe4a7747ee6d92464
Alex <acim650@gmail.com>
2024-08-17 12:44:26 -0700

    fixed columns for table

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -21,31 +21,120 @@ function DataTable() {
             })
     }, [])
 
-    const options = {
-        filterType: 'checkbox'
-    }
-
-    const getMuiTheme = () => createTheme({
-        components: {
-            MUIDataTable: {
-                styleOverrides: {
-                    root: {
-                        backgroundColor: 'red'
-                    }
-                }
+    const columns = [
+        {
+            name: 'Age',
+            options: {
+                filter: true
             }
-        }
-    })
+        },
+        {
+            name: 'Abdomen',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Ankle',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Biceps',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Wrist',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Thigh',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Neck',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Knee',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Height',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Hip',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Forearm',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Density',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Chest',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'BodyFat',
+            options: {
+                filter: true
+            }
+        },
+        {
+            name: 'Weight',
+            options: {
+                filter: true
+            }
+        },
+    ]
+
+    // const getMuiTheme = () => createTheme({
+    //     components: {
+    //         MUIDataTable: {
+    //             styleOverrides: {
+    //                 root: {
+    //                     backgroundColor: 'red'
+    //                 }
+    //             }
+    //         }
+    //     }
+    // })
 
     return (
         <div>
-            <ThemeProvider theme={getMuiTheme()}>
-                <MUIDataTable
-                    title={"Bodyfat Data"}
-                    data={data}
-                    columns={["id", "Density", "BodyFat", "Age", "Weight", "Height", "Neck", "Chest", "Abdomen", "Hip", "Thigh", "Knee", "Ankle", "Biceps", "Forearm", "Wrist"]}
-                />
-            </ThemeProvider>
+            {/* <ThemeProvider theme={getMuiTheme()}> */}
+            <MUIDataTable
+                title={"Bodyfat Data"}
+                data={data}
+                columns={columns}
+            />
+            {/* </ThemeProvider> */}
         </div>
     )
 }
