667f924cbb2a3e02885da2dd2558270b6a527140
Alex <acim650@gmail.com>
2024-11-04 15:55:23 -0800

    completed adduser function

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,14 +3,32 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import instance from '../helpers/axiosInstance'
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import { error } from 'console';
+
 
 interface UserData {
     name?: string | null;
 }
 
+interface AddUserData {
+    username?: string | null;
+    email?: string | null;
+    password?: string | null;
+}
+
 
 function page() {
     const [users, setUsers] = useState<UserData[]>([])
+    const [addUser, setAddUser] = useState<AddUserData>({
+        username: '',
+        email: '',
+        password: ''
+    })
+    const [clickUserButton, setClickUserButton] = useState(Boolean)
+
     useEffect(() => {
         instance.get('/get-users')
             .then(function (res) {
@@ -20,10 +38,83 @@ function page() {
 
     console.log("users", users)
 
+    const addUserButton = () => {
+        setClickUserButton(!clickUserButton)
+    }
+
+    const addUserInfo = () => {
+        instance.post('/register', {
+            username: addUser.username,
+            email: addUser.email,
+            password: addUser.password
+        })
+            .then(function (res) {
+                console.log("res", res)
+            })
+            .catch((error) => {
+                console.log("error", error)
+            })
+    }
+
+    const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAddUser({
+            ...addUser,
+            username: e.target.value
+        })
+    }
+
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAddUser({
+            ...addUser,
+            email: e.target.value
+        })
+    }
+
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAddUser({
+            ...addUser,
+            password: e.target.value
+        })
+    }
+
+
     return (
         <div>
             <div className='flex flex-row'>
-                <button className='bg-white text-black'>Add User</button>
+                <button className='bg-white text-black' onClick={addUserButton}>Add User</button>
+                {
+                    clickUserButton && (
+                        <div className='flex flex-col'>
+                            <TextField
+                                label="Username"
+                                variant="outlined"
+                                className="bg-white"
+                                onChange={handleUsername}
+                                value={addUser.username}
+                            >
+                            </TextField>
+                            <TextField
+                                label="Email"
+                                variant="outlined"
+                                className="bg-white"
+                                onChange={handleEmail}
+                                value={addUser.email}
+                            >
+                                Email
+                            </TextField>
+                            <TextField
+                                label="Password"
+                                variant="outlined"
+                                className='bg-white'
+                                onChange={handlePassword}
+                                value={addUser.password}
+                            >
+                                Password
+                            </TextField>
+                            <Button onClick={addUserInfo}>Add User</Button>
+                        </div>
+                    )
+                }
             </div>
             {users.map((user, index) => {
                 return (
